168aa18374dec5c409d6e47d5fa9ee6d0f479205
Damien Perkins <damien.perkins@ioof.com.au>
2017-01-21 19:18:09 +1100

    Fixed lobby [Damien P]

--- src/Lobby/Lobby.js
+++ src/Lobby/Lobby.js
@@ -5,24 +5,29 @@ import { Container, Button, Header, Segment, List } from 'semantic-ui-react'
 import * as Actions from '../actions'
 import { pickAColour } from '../util/pickAColour'
 
-const Lobby = ({ players }) => {
-  return (
-    <Container>
-      <Segment inverted color={pickAColour()} size={'massive'}>
-        <Header size={'huge'} textAlign={'center'}>Game ####</Header>
-      </Segment>
-      <Segment>
-        <List>
-          {
-            players.map(player => (<List.Item>{player.name}</List.Item>))
-          }
-        </List>
-      </Segment>
-      <Segment>
-        <Button>Start Game</Button>
-      </Segment>
-    </Container>
-  )
+class Lobby extends React.Component {
+  componentWillMount() {
+    this.props.getPlayerList()
+  }
+  render() {
+    return (
+      <Container>
+        <Segment inverted color={pickAColour()} size={'massive'}>
+          <Header size={'huge'} textAlign={'center'}>Game ####</Header>
+        </Segment>
+        <Segment>
+          <List>
+            {
+              this.props.playerList.map(player => (<List.Item>{player.name}</List.Item>))
+            }
+          </List>
+        </Segment>
+        <Segment>
+          <Button>Start Game</Button>
+        </Segment>
+      </Container>
+    )
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
--- src/actions.js
+++ src/actions.js
@@ -4,9 +4,9 @@ import { push } from 'react-router-redux'
 export const constants = {
   SET_PLAYER_ID: 'SET_PLAYER_ID',
   SET_PLAYER_NAME: 'SET_PLAYER_NAME',
-  GET_PLAYER_LIST: 'GET_PLAYER_LIST',
-  START_GAME: 'START_GAME',
   SET_ROOM_CODE: 'SET_ROOM_CODE',
+  SET_PLAYER_LIST: 'SET_PLAYER_LIST',
+  START_GAME: 'START_GAME',
 }
 
 export const setPlayerName = (value) => {
@@ -30,12 +30,21 @@ export const setRoomCode = (value) => {
   }
 }
 
+export const setPlayerList = (value) => {
+  return {
+    type: constants.SET_PLAYER_LIST,
+    value,
+  }
+}
+
 export const goToCreateGame = () => dispatch => dispatch(push('/create'))
 export const goToJoinGame = () => dispatch => dispatch(push('/join'))
 
 export const getPlayerList = () => (dispatch, getState) => {
   const state = getState()
   const url = `https://utopia-server.herokuapp.com/games/${state.room.roomCode}`
+  axios.get(url)
+       .then(response => dispatch(setPlayerList(response.data)))
 }
 
 export const joinGame = () => (dispatch, getState) => {
@@ -47,12 +56,11 @@ export const joinGame = () => (dispatch, getState) => {
 
 export const createGame = () => (dispatch, getState) => {
   const playerName = getState().playerName
-  console.log('player name: ', playerName)
   const url = 'https://utopia-server.herokuapp.com/games'
   axios.put(url, { playerName })
        .then((response) => {
          dispatch(setPlayerID(response.data._id))
          dispatch(setRoomCode(response.data.gameId))
        })
-       .then(() => dispatch(push('/lobby/:reference')))
+       .then(() => dispatch(push('/lobby')))
 }
